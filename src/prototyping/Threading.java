4f2a0acb8051712393c0e1219f42525b9ed5c1c1
Matt Mead <matthewmead1@live.co.uk>
2018-03-06 13:09:03 +0000

    Added a multithreading prototype.

---
+++ src/prototyping/Threading.java
@@ -0,0 +1,45 @@
+/*
+ * Meme Team Software Engineering Project
+ * Property Tycoon
+ */
+package prototyping;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author matth
+ */
+public class Threading extends Application
+{
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws InterruptedException
+    {
+        Thread backgroundLoader = new Thread(() -> {
+            try {
+                XmlParsing.main(null);
+            }
+            catch (Exception e) {
+                
+            }
+        });
+        
+        // Load background XML data in parallel
+        backgroundLoader.start();
+
+        // Display the GUI
+        stage.setTitle("Property Tycoon");
+        stage.show();
+        
+        // Wait for background data to finish loading
+        backgroundLoader.join();
+        
+        // Use data somehow
+    }
+}
