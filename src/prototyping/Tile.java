fe8c98f00a91695ccbeb2f8d572baf2aa3aa594a
adurm <36302797+adurm@users.noreply.github.com>
2018-03-06 13:07:08 +0000

    Create Tile.java

---
+++ src/prototyping/Tile.java
@@ -0,0 +1,20 @@
+package prototyping;
+
+/**
+ *
+ * @author adam
+ * Class Tile represents a square on the game board
+ */
+public class Tile {
+    
+    //The property on the tile
+    private Property property;
+    
+    public void setProperty(Property property){
+        this.property = property;
+    }
+    
+    public Property getProperty(){
+        return property;
+    }
+}
